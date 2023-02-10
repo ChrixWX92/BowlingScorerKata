58824499845d513adc71446272f50af19d60e277
ChrixWX92 <chris@wards-net.co.uk>
2023-02-10 19:09:25 +0000

    fix: Additional logic tweaks

--- src/bowlingscorer/GUI.java
+++ src/bowlingscorer/GUI.java
@@ -1,15 +1,12 @@
 package bowlingscorer;
 
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
@@ -20,18 +17,17 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public class GUI {
 
     Game game;
+    boolean gameOver = false;
     int frameCount;
     private static List<GridPane> frames;
     final int[] frameCounter = {0};
     final int[] ballCounter = {0};
+    private int deferCount = 0;
 
     public GUI(Game game) {
 
@@ -60,6 +56,8 @@ public class GUI {
 
         scoreInput.textProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue.matches("\\d*")) {
+                // TODO: Add functionality to accept X, x, - and /
+                // TODO: No logic for valid score or spare functionality
                 scoreInput.setText(newValue.replaceAll("[^\\d]", ""));
             }
         });
@@ -77,27 +75,28 @@ public class GUI {
         borderPane.setBottom(buttons);
         borderPane.setTop(frames);
 
-        button.setOnAction(new EventHandler() {
-            @Override
-            public void handle(Event event) {
+        button.setOnAction(event -> {
+            if (!gameOver) {
                 game.play();
                 addGraphic();
             }
         });
 
-        button2.setOnAction(new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                game.play(Integer.parseInt(String.valueOf(scoreInput.getCharacters())));
-                addGraphic();
+        button2.setOnAction(event -> {
+            if (!gameOver) {
+                int value = Integer.parseInt(String.valueOf(scoreInput.getCharacters()));
+                if (value > 10 || value < 0) {
+                    scoreInput.setText("");
+                    //TODO: Explain invalid input to user
+                } else {
+                    game.play(Integer.parseInt(String.valueOf(scoreInput.getCharacters())));
+                    addGraphic();
+                }
             }
         });
 
-        button3.setOnAction(new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                // TODO: Reset all params
-            }
+        button3.setOnAction(event -> {
+            // TODO: Reset all params
         });
 
 
@@ -110,7 +109,7 @@ public class GUI {
         Frame[] threadFrames = game.getFrames();
         Frame currentFrame = threadFrames[frameCounter[0]];
 
-        System.out.println(String.valueOf(currentFrame.getBalls()[ballCounter[0]]));
+        System.out.println("\n" + String.valueOf(currentFrame.getBalls()[ballCounter[0]]));
         String text = "";
         if (frameCounter[0] < threadFrames.length) text = String.valueOf(currentFrame.getBalls()[ballCounter[0]]);
 
@@ -119,16 +118,57 @@ public class GUI {
                 text = "-";
             }
             case "10" -> {
-                text = "X";
+                // Setting strike symbol and skipping total
+                ((Label) GUI.frames.get(frameCounter[0]).getChildren().get(1)).setText("X");
+                if (threadFrames[frameCounter[0]].getType() == Frame.FrameType.FINAL) {
+                    ballCounter[0]++;
+                    if (ballCounter[0] == 3) {
+                        gameOver = true;
+                    }
+                }
+                else {
+                    ballCounter[0] = 0;
+                    frameCounter[0]++;
+                    deferCount += 2;
+                }
+                return;
             }
             case "null" -> {
                 text = " ";
 
             }
             default -> {
-                if (currentFrame.sumBalls() == 10 && currentFrame.getType() != Frame.FrameType.FINAL) text = "/";
+                if (ballCounter[0] > 0 && currentFrame.sumBalls() == 10 && currentFrame.getType() != Frame.FrameType.FINAL) {
+                    ((Label) GUI.frames.get(frameCounter[0]).getChildren().get(2)).setText("/");
+                    ballCounter[0] = 0;
+                    frameCounter[0]++;
+                    deferCount += 1;
+                    return;
+                }
             }
         }
+
+        System.out.printf("defer = " + deferCount + " | ");
+        if (deferCount == 1) {
+            System.out.println("frameCOunter = " + frameCounter[0]);
+            for (int i = 0 ; i < frameCounter[0] ; i++) {
+                int score = -1;
+                if (((Label) GUI.frames.get(i).getChildren().get(4)).getText().length() > 0) {
+                    try {
+                        score = Integer.parseInt(((Label) GUI.frames.get(i).getChildren().get(4)).getText());
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("score = " + score + " --- frame total to " + i + " = " + game.calculateSetFrameTotal(i));
+                if (((Label) GUI.frames.get(i).getChildren().get(4)).getText().length() > 0 || score != game.calculateSetFrameTotal(i)) {
+                    ((Label) GUI.frames.get(i).getChildren().get(4)).setText(String.valueOf(game.calculateSetFrameTotal(i)));
+                } else {
+                    System.out.println("frame " + i + " score equal");
+                }
+            }
+        }
+        if (deferCount > 0) deferCount--;
 
         if (frameCounter[0] == frameCount - 1) {
             System.out.println("FINAL");
@@ -137,40 +177,31 @@ public class GUI {
                 case 0 -> ((Label) GUI.frames.get(frameCounter[0]).getChildren().get(3)).setText(text);
                 case 1 -> ((Label) GUI.frames.get(frameCounter[0]).getChildren().get(1)).setText(text);
                 case 2 -> {
+                    //TODO: If a third ball hasn't been earned, this needs to trigger automatically
                     if (currentFrame.getBalls()[2] != null){
                         ((Label) GUI.frames.get(frameCounter[0]).getChildren().get(2)).setText(text);
                     }
                     else {
                         ((Label) GUI.frames.get(frameCounter[0]).getChildren().get(2)).setText("-");
                     }
+                    ((Label) GUI.frames.get(frameCounter[0]).getChildren().get(4)).setText(String.valueOf(game.getTotal()));
+                    gameOver = true;
                 }
+                default -> {return;}
             }
             ballCounter[0]++;
         }
         else {
-            ((Label) GUI.frames.get((frameCounter[0])).getChildren().get(ballCounter[0]+1)).setText(text);
+            ((Label) GUI.frames.get(frameCounter[0]).getChildren().get(ballCounter[0]+1)).setText(text);
             if (ballCounter[0] == 0) ballCounter[0]++;
             else {
-                ((Label) GUI.frames.get((frameCounter[0])).getChildren().get(4)).setText(String.valueOf(game.getTotal()));
+                ((Label) GUI.frames.get(frameCounter[0]).getChildren().get(4)).setText(String.valueOf(game.getTotal()));
                 ballCounter[0] = 0;
                 frameCounter[0]++;
 
             }
         }
 
-
-        // TODO: Needs rewriting
-//        for (int i = 0 ; i < frameCounter[0] ; i++) {
-//            int score = -1;
-//            try {
-//                score = Integer.parseInt(((Label) GUI.frames.get((frameCounter[0])).getChildren().get(3)).getText());
-//            } catch (NumberFormatException e) {
-//                e.printStackTrace();
-//            }
-//            if (score != game.calculateSetFrameTotal(i)) {
-//                ((Label) GUI.frames.get((frameCounter[0]-1)).getChildren().get(3)).setText(String.valueOf(game.calculateSetFrameTotal(i)));
-//            }
-//        }
     }
 
     public GridPane addFramePane(boolean finalFrame, int number) {
@@ -180,12 +211,10 @@ public class GUI {
         grid.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(5), Insets.EMPTY)));
 
         Border standardBorder = new Border(new BorderStroke(Paint.valueOf("Black"), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.THIN));
-//        grid.setHgap(10);
-//        grid.setVgap(10);
         grid.setBorder(new Border(new BorderStroke(Paint.valueOf("Black"), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.THIN)));
 
         Label numberBox = new Label();
-        numberBox.setFont(Font.font("Arial", FontWeight.BOLD, 25));
+        numberBox.setFont(Font.font("Tahoma", FontWeight.BOLD, 25));
         numberBox.setMinSize(35,35);
         numberBox.setMaxSize(35,35);
         numberBox.setText(String.valueOf(number));
@@ -193,21 +222,21 @@ public class GUI {
         grid.add(numberBox, 1, 0);
 
         Label ball1 = new Label();
-        ball1.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        ball1.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
         ball1.setBorder(standardBorder);
         ball1.setMinSize(35,35);
         ball1.setAlignment(Pos.CENTER);
         grid.add(ball1, 1, 1);
 
         Label ball2 = new Label();
-        ball2.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        ball2.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
         ball2.setBorder(standardBorder);
         ball2.setMinSize(35,35);
         ball2.setAlignment(Pos.CENTER);
         grid.add(ball2, 2, 1);
 
         Label ball3 = new Label();
-        ball3.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        ball3.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
         ball3.setMinSize(35,35);
         ball3.setBorder(standardBorder);
         ball3.setAlignment(Pos.CENTER);
@@ -217,7 +246,7 @@ public class GUI {
         // Total box
         Label total = new Label();
 //        GridPane.setValignment(total, VPos.BOTTOM);
-        total.setFont(Font.font("Arial", FontWeight.BOLD, 25));
+        total.setFont(Font.font("Tahoma", FontWeight.BOLD, 25));
         total.setMinSize(0,35);
         total.setAlignment(Pos.CENTER);
         grid.add(total, 0, 2);
